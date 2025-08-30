14f2a3c011624555b56b558e24ee3212698fe386
zcdev <zcdev@hotmail.com>
2025-08-30 08:55:37 -0700

    feat: implement app page and feature project content with MDX metadata utility and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,40 @@
 import Link from "next/link";
-import ProjectsList from "./components/projectList";
-import Projects from "./components/project";
-
-const showcases = [
-  {
-    title: "Who Ate the Cookie 🍪",
-    features: "Next.js + Voice API",
-    description:
-      "A playful, voice-interactive guessing game built with Next.js. Designed for accessibility and performance, with an engaging narrative that keeps users curious from start to finish.",
-  },
-  {
-    title: "Easter Eggs Festival 🐣",
-    features: "React.js + TypeScript",
-    description:
-      "A strategic number-memory game built in React.js. Features custom sound design and inclusive accessibility practices, creating a fun, approachable experience for all ages.",
-  },
-  {
-    title: "Easter Eggs Festival 🐣",
-    features: "React.js + TypeScript",
-    description:
-      "A strategic number-memory game built in React.js. Features custom sound design and inclusive accessibility practices, creating a fun, approachable experience for all ages.",
-  },
-];
+import ProjectIndex from "./projects/page";
+import { RxArrowRight } from "react-icons/rx";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-8 gap-x-8 gap-y-6 min-h-screen bg-white text-gray-900 p-8">
+    <main className="grid grid-cols-8 gap-x-8 gap-y-6 min-h-screen bg-white text-gray-900 p-8 dark:bg-stone-950 dark:text-zinc-400">
       {/* Hero */}
-      <aside className="text-center mb-16 col-span-2">
+      <aside className="mb-16 col-span-2">
         <section className="text-left">
           <h1 className="text-4xl font-bold mb-2">Zoe Chang</h1>
-          <p className="text-xl text-purple-600 mb-4">
-            Frontend Engineer & Digital Creative
+          <p className="text-xl text-purple-600 mb-1 dark:text-purple-400">
+            Front-end Developer
+          </p>
+          <p className="text-base font-semibold mb-4">
+            Building accessible and interactive UX
           </p>
           <p className="text-base mb-4">
-            I’m a frontend developer and digital creative with roots in design,
-            music production, and media. I specialize in React.js and modern
-            JavaScript frameworks, blending artistry with engineering to bring
-            design visions to life as interactive, user-focused products.
+            I’m a front-end developer specializing in crafting modern,
+            responsive web interfaces with a strong emphasis on accessibility,
+            performance, and intuitive user interaction. With hands-on
+            experience in React, Next.js, and TypeScript, I build products that
+            are both usable and engaging.
           </p>
           <p className="text-base mb-4">
-            My background gives me a unique perspective: I think like a
-            designer, build like an engineer, and create experiences that feel
-            both functional and expressive. From enterprise platforms to playful
-            UI experiments, I thrive on translating ideas into polished,
-            engaging interfaces.
+            All the web apps in my portfolio support accessibility, dark-mode,
+            and responsive designs.
           </p>
         </section>
         {/* Blog Teaser */}
         <section className="text-left">
           <h2 className="text-2xl font-semibold mb-4">Latest Updates</h2>
-          <Link href="/blog" className="text-purple-600 hover:underline">
-            Visit Blog →
+          <Link
+            href="/blog"
+            className="text-lg text-purple-600 dark:text-purple-400 hover:underline align-middle inline-block"
+          >
+            Visit Blog <RxArrowRight className="icon-inline align-[-0.24em]" />
           </Link>
         </section>
       </aside>
@@ -59,11 +42,7 @@ export default function Home() {
       {/* Featured Projects */}
       <section className="mb-16 col-span-6">
         <h2 className="text-2xl font-semibold mb-4">Featured Projects</h2>
-        <ProjectsList>
-          {showcases.map((showcase, index) => (
-            <Projects key={index} showcase={showcase} />
-          ))}
-        </ProjectsList>
+        <ProjectIndex />
       </section>
     </main>
   );
---
+++ src/app/utils/index-metadata.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export type MdxMeta = {
+  slug: string;
+  title: string;
+  description?: string;
+  tech?: string[];
+  url?: string;
+  codebase?: string;
+  date?: string;
+  cover?: string;
+};
+
+export function getPostMetadata(contentFolder: "docs" | "posts"): MdxMeta[] {
+  const dir = path.join(process.cwd(), `src/app/content/${contentFolder}`);
+  const files = fs.readdirSync(dir).filter(files => (files.endsWith(".mdx")));
+
+  return files.map(file => {
+    const source = fs.readFileSync(path.join(dir, file), "utf-8");
+    const { data } = matter(source);
+    const slug = (data.slug as string) || file.replace(/\.mdx$/, "");
+
+    return {
+      slug,
+      title: (data.title as string) || slug,
+      description: (data.description as string) || undefined,
+      tech: (data.tech as string[]) || undefined,
+      url: (data.url as string) || undefined,
+      codebase: (data.codebase as string) || undefined,
+      date: (data.date as string) || undefined,
+      cover: (data.cover as string) || undefined,
+    }
+  })
+}
+
+export const getProjectMeta = () => getPostMetadata("docs");
+export const getBlogMeta = () => getPostMetadata("posts");
