52d87d196ef411491cfbf1d5e8d7289de46b1875
zcdev <zcdev@hotmail.com>
2025-09-09 13:19:48 -0700

    feat(blog): sort posts with reverse chronological order, list dates, and group by years

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,35 +5,45 @@ import { RxArrowLeft } from "react-icons/rx";
 export default async function BlogIndex() {
   const posts = getBlogMeta();
 
+  const grouped = posts.reduce((total, post) => {
+    const year = new Date(post.date!).getFullYear();
+    if (!total[year]) total[year] = [];
+    total[year].push(post);
+    return total;
+  }, {} as Record<number, typeof posts>);
+
   return (
     <main className="prose max-w-2xl text-left p-8">
       <header>
         <h1 className="text-4xl font-bold mb-6">Blog</h1>
         <h2 className="text-lg font-semibold mb-4">Real lessons, light humor — coding notes with a human touch.</h2>
-        <p className="text-lg mb-4">These posts are my reflections, jotted down with witty humor and a playful twist as I explore and uncover insights through coding trials and errors. They capture lessons learned from my projects, explained in plain language with a friendly, human touch. Along the way, you may even find the takeaways amusing! 😏✨</p>
+        <p className="text-lg mb-5">These posts are my reflections, jotted down with witty humor and a playful twist as I explore and uncover insights through coding trials and errors. They capture lessons learned from my projects, explained in plain language with a friendly, human touch. Along the way, you may even find the takeaways amusing! 😏✨</p>
       </header>
-      <section>
-        <ul className="mb-12">
-          {posts.map((post) => {
-            return (
-              <li key={post.slug} className="mb-2">
-                <Link
-                  href={`/blog/${post.slug}`}
-                  className="text-lg text-purple-600 hover:underline dark:text-purple-400"
-                >
-                  {post.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        <Link
-          href="/"
-          className="text-lg text-purple-600 dark:text-purple-400 hover:underline inline-block"
-        >
-          <RxArrowLeft className="icon-inline align-[-0.17em]" /> Back to Home
-        </Link>
-      </section>
+      {Object.keys(grouped)
+        .sort((a, b) => Number(b) - Number(a))
+        .map((year) => (
+          <section key={year}>
+            <h3 className="text-lg font-semibold mb-1">{year}</h3>
+            <ul className="mb-6">
+              {grouped[Number(year)].map((post) => (
+                <li key={post.slug} className="mb-2">
+                  <Link
+                    href={`/blog/${post.slug}`}
+                    className="text-lg text-purple-600 hover:underline dark:text-purple-400"
+                  >
+                    <span className="text-gray-600">{post.date!.slice(5, post.date?.length)}:</span> {post.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+        ))}
+      <Link
+        href="/"
+        className="text-lg text-purple-600 dark:text-purple-400 hover:underline inline-block"
+      >
+        <RxArrowLeft className="icon-inline align-[-0.17em]" /> Back to Home
+      </Link>
     </main>
   );
 }
