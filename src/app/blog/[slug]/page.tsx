4f5fe9c2bbdeb620921a8e99371455892de5e4cf
zcdev <zcdev@hotmail.com>
2025-09-09 16:55:17 -0700

    fix: drop params from child component and make sure that it doesn't reference params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import MDXPage from "../../components/MDXPage";
 
 export default function BlogPostPage({ params }: { params: { slug: string; }; }) {
-  const { slug } = params;
-  return <MDXPage slug={slug} contentFolder="posts" />;
+  return <MDXPage slug={params.slug} contentFolder="posts" />;
 }
--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import MDXPage from "../../components/MDXPage";
 
 export default function ProjectPage({ params }: { params: { slug: string; }; }) {
-  const { slug } = params;
-  return <MDXPage slug={slug} contentFolder="docs" />;
+  return <MDXPage slug={params.slug} contentFolder="docs" />;
 }
