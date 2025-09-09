c835b1bbd18c00f6b0bf131145b9d40bbc9aae25
zcdev <zcdev@hotmail.com>
2025-09-09 16:58:13 -0700

    fix: cast dynamic route params to { slug: string } to unblock TypeScript error

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import MDXPage from "../../components/MDXPage";
 
 export default function BlogPostPage({ params }: { params: { slug: string; }; }) {
-  return <MDXPage slug={params.slug} contentFolder="posts" />;
+  return <MDXPage slug={(params as { slug: string; }).slug} contentFolder="posts" />;
 }
--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import MDXPage from "../../components/MDXPage";
 
 export default function ProjectPage({ params }: { params: { slug: string; }; }) {
-  return <MDXPage slug={params.slug} contentFolder="docs" />;
+  return <MDXPage slug={(params as { slug: string; }).slug} contentFolder="docs" />;
 }
