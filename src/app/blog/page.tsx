0ab82235d9ee9d9348da060f62b48241d5bc965c
zcdev <zcdev@hotmail.com>
2025-09-09 17:20:36 -0700

    refactor(blog): unlink dates from post entries

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -27,11 +27,11 @@ export default async function BlogIndex() {
             <ul className="mb-6">
               {grouped[Number(year)].map((post) => (
                 <li key={post.slug} className="mb-2">
+                  <span className="text-lg text-gray-600 pr-2">{post.date!.slice(5, post.date?.length)}:</span>
                   <Link
                     href={`/blog/${post.slug}`}
                     className="text-lg text-purple-600 hover:underline dark:text-purple-400"
-                  >
-                    <span className="text-gray-600">{post.date!.slice(5, post.date?.length)}:</span> {post.title}
+                  >{post.title}
                   </Link>
                 </li>
               ))}
