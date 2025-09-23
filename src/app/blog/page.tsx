7304955af7bab115171b3338cbfefba068080529
zcdev <zcdev@hotmail.com>
2025-09-23 15:03:50 -0700

    feat(blog): sort posts by reverse chronological order and refine group by year

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
-import { getBlogMeta } from "../utils/index-metadata";
+import { getBlogMeta, MdxMeta } from "../utils/index-metadata";
 import { RxArrowLeft } from "react-icons/rx";
 
 export default async function BlogIndex() {
   const posts = getBlogMeta();
 
+  // Group posts by year
   const grouped = posts.reduce((total, post) => {
     const year = new Date(post.date!).getFullYear();
     if (!total[year]) total[year] = [];
-    total[year].push(post);
     return total;
   }, {} as Record<number, typeof posts>);
+
+  // Sort posts by reverse chronological order
+  const sorted = posts.sort((a: any, b: any) => Number(new Date((b.date))) - Number(new Date(a.date)));
 
   return (
     <main className="prose max-w-2xl text-left p-8">
@@ -22,17 +25,17 @@ export default async function BlogIndex() {
       {Object.keys(grouped)
         .sort((a, b) => Number(b) - Number(a))
         .map((year) => (
-          <section key={year}>
+          <section>
             <h3 className="text-lg font-semibold mb-1">From {year}</h3>
             <ul className="mb-6">
-              {grouped[Number(year)].map((post) => (
+              {sorted.map((post: any) => (
                 <li key={post.slug} className="mb-2">
                   <Link
                     href={`/blog/${post.slug}`}
                     className="text-lg text-purple-600 hover:underline dark:text-purple-400"
                   >{post.title}
                   </Link>
-                  <span className="text-lg text-gray-400 pl-2">({post.date!.slice(5, post.date?.length)})</span>
+                  <span className="text-lg text-gray-400 pl-2">({post.date.slice(5, post.date.length)})</span>
                 </li>
               ))}
             </ul>
