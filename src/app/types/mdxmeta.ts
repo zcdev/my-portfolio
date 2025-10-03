758565292e8936fc0443d7010cc325465b5023db
zcdev <zcdev@hotmail.com>
2025-10-02 18:37:24 -0700

    refactor(mdxmeta): centralize type definition and re-import in utils

---
+++ src/app/types/mdxmeta.ts
@@ -0,0 +1,10 @@
+export type MdxMeta = {
+    id?: number;
+    slug: string;
+    title: string;
+    date?: string;
+    description?: string;
+    tech?: string[] | undefined;
+    demo?: string;
+    codebase?: string;
+};
